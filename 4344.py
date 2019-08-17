968284fbf9bb84c5aaa71fc1b516f56065ffb29f
Im SeongChang <pabi3564@gmail.com>
2019-08-17 17:27:01 +0900

    solved

--- 4344.py
+++ 4344.py
@@ -2,6 +2,14 @@ t = int(input())
 li = list()
 
 for i in range(0,t):
-    li = int(input().split())
-    avg = sum(li, 0.00)
-    print(str(avg) + "%")
+    li = input().split()
+    su = 0
+    for j in range(1, int(li[0])+1):
+        su += int(li[j])
+    avg = su / int(li[0])
+    cnt = 0
+    res = 0
+    for j in range(1, int(li[0])+1):
+        if avg < int(li[j]) : cnt += 1
+    res = round(cnt / int(li[0]) * 100 , 3)
+    print('%.3f' % res + "%")
